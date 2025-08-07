329b4a98793f8fbc0734779d94e336a50313c7c7
Yulmio <sr.yulmio@gmail.com>
2025-08-07 21:18:41 +0200

    Primer commit

---
+++ Calculadora/Calculadora.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		//calculadora
+		System.out.println("Escriba el numero indicado");
+		System.out.println("////////////////////////////////////");
+		System.out.println("Opcione 1: Suma");
+		System.out.println("Opcione 2: Resta");
+		System.out.println("Opcione 3: multiplicaciones");
+		System.out.println("Opcione 4: diviciones");
+		System.out.println("////////////////////////////////////");
+		int pregunta1Num = Integer.parseInt(JOptionPane.showInputDialog("Introsca el primer digito: "));
+		int pregunta2Num = Integer.parseInt(JOptionPane.showInputDialog("Introsca el segundo digito: "));
+
+
+
+
+
+	}
+
+}
