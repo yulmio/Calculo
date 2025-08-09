d2412d5b179c380eef04d17ebd7fa099d4869558
Yulmio <sr.yulmio@gmail.com>
2025-08-09 23:15:19 +0200

    Introduje en la calculadora el calculo de las raíces cuadradas

---
+++ Calculadora/Abrir.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/yulmio/Escritorio/Calculo/Calculadora
+gnome-terminal -- bash -c "/home/yulmio/JavaVersiones/jdk-24.0.2/bin/java Calculadora; exec bash"
+
--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -3,52 +3,56 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		System.out.println("");
-		JOptionPane.showMessageDialog(null, "Opcion 1: Sumar\nOpcion 2:  Restar\nOpcion 3: Multiplicar\nOpcion 4: Dividir\nOpcion 5: Resto\nOpcion 6: Potencia");
-		int peticion = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el numero de opcion de la operacion ritmetica que deseas: "));
+		JOptionPane.showMessageDialog(null, "Opción 1: Sumar\nOpción 2:  Restar\nOpción 3: Multiplicar\nOpción 4: Dividir\nOpción 5: Resto\nOpción 6: Potencia\nOpción 7: Raíces cuadradas");
+		int peticion = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el numero de opción de la operación aritmética que deseas: "));
 		
 		if (peticion == 1) {
-			int peticionS1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Sumar, introduce el digito"));
+			int peticionS1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Sumar, introduce el dígito"));
 			int peticionS2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Sumar, introduce el segundo"));
 			int peticionFinal = Math.abs(peticionS1 + peticionS2);
 			JOptionPane.showMessageDialog(null, "El resultado es : " + peticionFinal);
 		}
 		
 		if (peticion == 2) {
-			int peticionR1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Restar, introduce el digito"));
+			int peticionR1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Restar, introduce el dígito"));
 			int peticionR2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Restar, introduce el segundo"));
 			int peticionFinal = Math.abs(peticionR1 - peticionR2);
 			JOptionPane.showMessageDialog(null, "El resultado es : " + peticionFinal);
 		}
 		
 		if (peticion == 3) {
-			int peticionM1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Multiplicar, introduce el digito"));
+			int peticionM1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Multiplicar, introduce el dígito"));
 			int peticionM2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Multiplicar, introduce el segundo"));
 			int peticionFinal = Math.abs(peticionM1 * peticionM2);
 			JOptionPane.showMessageDialog(null, "El resultado es : " + peticionFinal);
 		}
 		
 		if (peticion == 4) {
-			int peticionD1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Dividir, introduce el digito"));
+			int peticionD1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Dividir, introduce el dígito"));
 			int peticionD2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Dividir, introduce el segundo"));
 			int peticionFinal = Math.abs(peticionD1 / peticionD2);
 			JOptionPane.showMessageDialog(null, "El resultado es : " + peticionFinal);
 		}
 		
 		if (peticion == 5) {
-			int peticionRE1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Resto, introduce el digito"));
+			int peticionRE1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Resto, introduce el dígito"));
 			int peticionRE2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Seleccionaste Resto, introduce el segundo"));
 			int peticionFinal = Math.abs(peticionRE1 % peticionRE2);
 			JOptionPane.showMessageDialog(null, "El resultado es : " + peticionFinal);
 		}
 		
 		if (peticion == 6) {
-			double peticionP1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Seleccionaste Potencia, introduce el digito BASE"));
+			double peticionP1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Seleccionaste Potencia, introduce el dígito BASE"));
 			double peticionP2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Seleccionaste Potencia, introduce el segundo EXPONENTE"));
 			int peticionFinal = (int)Math.pow(peticionP1, peticionP2);
 			JOptionPane.showMessageDialog(null, "El resultado es : " + peticionFinal);
 		}
 		
-		
+		if (peticion == 7) {
+			double peticionPO1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Seleccionaste Raíz cuadrada, introduce el dígito"));
+			int peticionFinal = (int)Math.sqrt(peticionPO1);
+			JOptionPane.showMessageDialog(null, "El resultado es : " + peticionFinal);
+		}
 		
 		
 		
